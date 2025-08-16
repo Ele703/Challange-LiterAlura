364c4276fba188cb34045afd034a59910307e854
Eleazar Lima <edrxdrdr@gmail.com>
2025-08-16 20:58:46 -0300

    Create autor.java

---
+++ autor.java
@@ -0,0 +1,47 @@
+package com.literalura.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Integer anoNascimento;
+    private Integer anoFalecimento;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Livro> livros = new ArrayList<>();
+}
+
+package com.literalura.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String idioma;
+    private Integer downloads;
+
+    @ManyToOne
+    private Autor autor;
+}
+
