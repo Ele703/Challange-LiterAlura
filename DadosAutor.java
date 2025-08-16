bcb4c18c9014afb45f6502f8541d2816517fa80f
Eleazar Lima <edrxdrdr@gmail.com>
2025-08-16 20:46:51 -0300

    Update DadosAutor.java

--- DadosAutor.java
+++ DadosAutor.java
@@ -8,3 +8,26 @@ public record DadosAutor(
         Integer birth_year,
         Integer death_year
 ) {}
+
+package com.literalura.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosLivro(
+        String title,
+        List<DadosAutor> authors,
+        List<String> languages,
+        int download_count
+) {}
+
+package com.literalura.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ResultadoAPI(
+        List<DadosLivro> results
+) {}
