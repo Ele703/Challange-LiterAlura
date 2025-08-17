bbba43bc3c56e084122541a76cf8ed9d320d9c2a
Eleazar Lima <edrxdrdr@gmail.com>
2025-08-16 21:05:43 -0300

    Update AutorRepository.java

---
+++ AutorRepository.java
@@ -0,0 +1,18 @@
+package com.literalura.repository;
+
+import com.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNome(String nome);
+}
+
+package com.literalura.repository;
+
+import com.literalura.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
